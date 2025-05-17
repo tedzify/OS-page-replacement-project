c344c5686833c6082f5344ad07f53dc9b1e11e8e
tedzify <vontedzify@gmail.com>
2025-05-18 01:35:06 +0800

    Add project files.

---
+++ MainProject/UI/Views/MainForm.cs
@@ -0,0 +1,10 @@
+namespace OS_page_replacement_project
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
