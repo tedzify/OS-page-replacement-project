4c306dc57041b3a1cae73544967ce651d10c02a0
tedzify <vontedzify@gmail.com>
2025-05-20 04:39:31 +0800

    Refactor Mainform layout and controls
    - removed: Eliminated the SplitContainer `splitMain`
    - feat: Added new panel `pnlVisualization` for display
    - feat: Reintroduced `pnlControlContainer` with controls
    - updated: Adjusted layout to position `pnlControlContainer` on the right
    - updated: Modified properties of various controls for improved UI

--- PageReplacement.UI/Forms/Mainform.Designer.cs
+++ PageReplacement.UI/Forms/Mainform.Designer.cs
@@ -28,65 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            splitMain = new SplitContainer();
-            pnlRefString = new Panel();
+            pnlVisualization = new Panel();
             lblRefString = new Label();
+            pnlRefString = new Panel();
             pnlBtnContainer = new Panel();
             gboxSelectedAlgo = new GroupBox();
             lblSelectedAlgo = new Label();
             btnOptimal = new Button();
             btnLRU = new Button();
             btnFIFO = new Button();
-            pnlControlContainer = new Panel();
-            btnGenerateString = new Button();
-            numSeqLength = new NumericUpDown();
-            lblSeqLength = new Label();
-            numFrameCount = new NumericUpDown();
             lblFrameCount = new Label();
-            pnlVisualization = new Panel();
-            ((System.ComponentModel.ISupportInitialize)splitMain).BeginInit();
-            splitMain.Panel1.SuspendLayout();
-            splitMain.Panel2.SuspendLayout();
-            splitMain.SuspendLayout();
+            numFrameCount = new NumericUpDown();
+            lblSeqLength = new Label();
+            numSeqLength = new NumericUpDown();
+            btnGenerateString = new Button();
+            pnlControlContainer = new Panel();
             pnlBtnContainer.SuspendLayout();
             gboxSelectedAlgo.SuspendLayout();
-            pnlControlContainer.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)numSeqLength).BeginInit();
             ((System.ComponentModel.ISupportInitialize)numFrameCount).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numSeqLength).BeginInit();
+            pnlControlContainer.SuspendLayout();
             SuspendLayout();
             // 
-            // splitMain
+            // pnlVisualization
             // 
-            splitMain.Dock = DockStyle.Fill;
-            splitMain.IsSplitterFixed = true;
-            splitMain.Location = new Point(0, 0);
-            splitMain.Name = "splitMain";
-            // 
-            // splitMain.Panel1
-            // 
-            splitMain.Panel1.BackColor = Color.FromArgb(64, 64, 64);
-            splitMain.Panel1.Controls.Add(pnlVisualization);
-            splitMain.Panel1.Controls.Add(lblRefString);
-            splitMain.Panel1.Controls.Add(pnlRefString);
-            splitMain.Panel1.Controls.Add(pnlBtnContainer);
-            // 
-            // splitMain.Panel2
-            // 
-            splitMain.Panel2.BackColor = Color.FromArgb(64, 64, 64);
-            splitMain.Panel2.Controls.Add(pnlControlContainer);
-            splitMain.Size = new Size(1184, 661);
-            splitMain.SplitterDistance = 910;
-            splitMain.TabIndex = 0;
-            // 
-            // pnlRefString
-            // 
-            pnlRefString.Anchor = AnchorStyles.Top;
-            pnlRefString.BackColor = Color.FromArgb(45, 45, 45);
-            pnlRefString.BorderStyle = BorderStyle.FixedSingle;
-            pnlRefString.Location = new Point(12, 114);
-            pnlRefString.Name = "pnlRefString";
-            pnlRefString.Size = new Size(895, 77);
-            pnlRefString.TabIndex = 4;
+            pnlVisualization.Anchor = AnchorStyles.Top;
+            pnlVisualization.BackColor = Color.FromArgb(45, 45, 45);
+            pnlVisualization.BorderStyle = BorderStyle.FixedSingle;
+            pnlVisualization.Location = new Point(12, 197);
+            pnlVisualization.Name = "pnlVisualization";
+            pnlVisualization.Size = new Size(895, 368);
+            pnlVisualization.TabIndex = 5;
             // 
             // lblRefString
             // 
@@ -101,6 +73,16 @@
             lblRefString.TabIndex = 5;
             lblRefString.Text = "Reference String";
             lblRefString.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pnlRefString
+            // 
+            pnlRefString.Anchor = AnchorStyles.Top;
+            pnlRefString.BackColor = Color.FromArgb(45, 45, 45);
+            pnlRefString.BorderStyle = BorderStyle.FixedSingle;
+            pnlRefString.Location = new Point(12, 114);
+            pnlRefString.Name = "pnlRefString";
+            pnlRefString.Size = new Size(895, 77);
+            pnlRefString.TabIndex = 4;
             // 
             // pnlBtnContainer
             // 
@@ -196,20 +178,55 @@
             btnFIFO.Text = "FIFO";
             btnFIFO.UseVisualStyleBackColor = false;
             // 
-            // pnlControlContainer
+            // lblFrameCount
             // 
-            pnlControlContainer.Anchor = AnchorStyles.Right;
-            pnlControlContainer.BackColor = Color.FromArgb(45, 45, 45);
-            pnlControlContainer.BorderStyle = BorderStyle.FixedSingle;
-            pnlControlContainer.Controls.Add(btnGenerateString);
-            pnlControlContainer.Controls.Add(numSeqLength);
-            pnlControlContainer.Controls.Add(lblSeqLength);
-            pnlControlContainer.Controls.Add(numFrameCount);
-            pnlControlContainer.Controls.Add(lblFrameCount);
-            pnlControlContainer.Location = new Point(3, 12);
-            pnlControlContainer.Name = "pnlControlContainer";
-            pnlControlContainer.Size = new Size(255, 637);
-            pnlControlContainer.TabIndex = 0;
+            lblFrameCount.AutoSize = true;
+            lblFrameCount.BackColor = Color.Transparent;
+            lblFrameCount.Font = new Font("Roboto Black", 12F);
+            lblFrameCount.ForeColor = Color.White;
+            lblFrameCount.Location = new Point(16, 19);
+            lblFrameCount.Name = "lblFrameCount";
+            lblFrameCount.Size = new Size(101, 19);
+            lblFrameCount.TabIndex = 1;
+            lblFrameCount.Text = "Frame count";
+            // 
+            // numFrameCount
+            // 
+            numFrameCount.BackColor = Color.Gray;
+            numFrameCount.BorderStyle = BorderStyle.None;
+            numFrameCount.Font = new Font("Roboto Black", 12F);
+            numFrameCount.ForeColor = Color.White;
+            numFrameCount.Location = new Point(19, 40);
+            numFrameCount.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
+            numFrameCount.Name = "numFrameCount";
+            numFrameCount.Size = new Size(214, 23);
+            numFrameCount.TabIndex = 1;
+            numFrameCount.TextAlign = HorizontalAlignment.Center;
+            // 
+            // lblSeqLength
+            // 
+            lblSeqLength.AutoSize = true;
+            lblSeqLength.BackColor = Color.Transparent;
+            lblSeqLength.Font = new Font("Roboto Black", 12F);
+            lblSeqLength.ForeColor = Color.White;
+            lblSeqLength.Location = new Point(16, 93);
+            lblSeqLength.Name = "lblSeqLength";
+            lblSeqLength.Size = new Size(131, 19);
+            lblSeqLength.TabIndex = 3;
+            lblSeqLength.Text = "Sequence length";
+            // 
+            // numSeqLength
+            // 
+            numSeqLength.BackColor = Color.Gray;
+            numSeqLength.BorderStyle = BorderStyle.None;
+            numSeqLength.Font = new Font("Roboto Black", 12F);
+            numSeqLength.ForeColor = Color.WhiteSmoke;
+            numSeqLength.Location = new Point(19, 114);
+            numSeqLength.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
+            numSeqLength.Name = "numSeqLength";
+            numSeqLength.Size = new Size(214, 23);
+            numSeqLength.TabIndex = 2;
+            numSeqLength.TextAlign = HorizontalAlignment.Center;
             // 
             // btnGenerateString
             // 
@@ -228,65 +245,20 @@
             btnGenerateString.Text = "GENERATE";
             btnGenerateString.UseVisualStyleBackColor = false;
             // 
-            // numSeqLength
+            // pnlControlContainer
             // 
-            numSeqLength.BackColor = Color.Gray;
-            numSeqLength.BorderStyle = BorderStyle.None;
-            numSeqLength.Font = new Font("Roboto Black", 12F);
-            numSeqLength.ForeColor = Color.WhiteSmoke;
-            numSeqLength.Location = new Point(19, 114);
-            numSeqLength.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
-            numSeqLength.Name = "numSeqLength";
-            numSeqLength.Size = new Size(214, 23);
-            numSeqLength.TabIndex = 2;
-            numSeqLength.TextAlign = HorizontalAlignment.Center;
-            // 
-            // lblSeqLength
-            // 
-            lblSeqLength.AutoSize = true;
-            lblSeqLength.BackColor = Color.Transparent;
-            lblSeqLength.Font = new Font("Roboto Black", 12F);
-            lblSeqLength.ForeColor = Color.White;
-            lblSeqLength.Location = new Point(16, 93);
-            lblSeqLength.Name = "lblSeqLength";
-            lblSeqLength.Size = new Size(131, 19);
-            lblSeqLength.TabIndex = 3;
-            lblSeqLength.Text = "Sequence length";
-            // 
-            // numFrameCount
-            // 
-            numFrameCount.BackColor = Color.Gray;
-            numFrameCount.BorderStyle = BorderStyle.None;
-            numFrameCount.Font = new Font("Roboto Black", 12F);
-            numFrameCount.ForeColor = Color.White;
-            numFrameCount.Location = new Point(19, 40);
-            numFrameCount.Maximum = new decimal(new int[] { 12, 0, 0, 0 });
-            numFrameCount.Name = "numFrameCount";
-            numFrameCount.Size = new Size(214, 23);
-            numFrameCount.TabIndex = 1;
-            numFrameCount.TextAlign = HorizontalAlignment.Center;
-            // 
-            // lblFrameCount
-            // 
-            lblFrameCount.AutoSize = true;
-            lblFrameCount.BackColor = Color.Transparent;
-            lblFrameCount.Font = new Font("Roboto Black", 12F);
-            lblFrameCount.ForeColor = Color.White;
-            lblFrameCount.Location = new Point(16, 19);
-            lblFrameCount.Name = "lblFrameCount";
-            lblFrameCount.Size = new Size(101, 19);
-            lblFrameCount.TabIndex = 1;
-            lblFrameCount.Text = "Frame count";
-            // 
-            // pnlVisualization
-            // 
-            pnlVisualization.Anchor = AnchorStyles.Top;
-            pnlVisualization.BackColor = Color.FromArgb(45, 45, 45);
-            pnlVisualization.BorderStyle = BorderStyle.FixedSingle;
-            pnlVisualization.Location = new Point(12, 197);
-            pnlVisualization.Name = "pnlVisualization";
-            pnlVisualization.Size = new Size(895, 368);
-            pnlVisualization.TabIndex = 5;
+            pnlControlContainer.Anchor = AnchorStyles.Right;
+            pnlControlContainer.BackColor = Color.FromArgb(45, 45, 45);
+            pnlControlContainer.BorderStyle = BorderStyle.FixedSingle;
+            pnlControlContainer.Controls.Add(btnGenerateString);
+            pnlControlContainer.Controls.Add(numSeqLength);
+            pnlControlContainer.Controls.Add(lblSeqLength);
+            pnlControlContainer.Controls.Add(numFrameCount);
+            pnlControlContainer.Controls.Add(lblFrameCount);
+            pnlControlContainer.Location = new Point(917, 12);
+            pnlControlContainer.Name = "pnlControlContainer";
+            pnlControlContainer.Size = new Size(255, 637);
+            pnlControlContainer.TabIndex = 0;
             // 
             // MainForm
             // 
@@ -294,43 +266,42 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(64, 64, 64);
             ClientSize = new Size(1184, 661);
-            Controls.Add(splitMain);
+            Controls.Add(pnlVisualization);
+            Controls.Add(lblRefString);
+            Controls.Add(pnlRefString);
+            Controls.Add(pnlBtnContainer);
+            Controls.Add(pnlControlContainer);
             MaximizeBox = false;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Page Replacement Algorithm";
-            splitMain.Panel1.ResumeLayout(false);
-            splitMain.Panel1.PerformLayout();
-            splitMain.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)splitMain).EndInit();
-            splitMain.ResumeLayout(false);
             pnlBtnContainer.ResumeLayout(false);
             gboxSelectedAlgo.ResumeLayout(false);
             gboxSelectedAlgo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numFrameCount).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numSeqLength).EndInit();
             pnlControlContainer.ResumeLayout(false);
             pnlControlContainer.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)numSeqLength).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numFrameCount).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private SplitContainer splitMain;
         private Panel pnlBtnContainer;
-        private Panel pnlControlContainer;
         private Button btnFIFO;
         private Button btnLRU;
         private Button btnOptimal;
         private GroupBox gboxSelectedAlgo;
         private Label lblSelectedAlgo;
-        private Label lblFrameCount;
-        private NumericUpDown numFrameCount;
-        private NumericUpDown numSeqLength;
-        private Label lblSeqLength;
-        private Button btnGenerateString;
         private Panel pnlRefString;
         private Label lblRefString;
         private Panel pnlVisualization;
+        private Panel pnlControlContainer;
+        private Button btnGenerateString;
+        private NumericUpDown numSeqLength;
+        private Label lblSeqLength;
+        private NumericUpDown numFrameCount;
+        private Label lblFrameCount;
     }
 }
